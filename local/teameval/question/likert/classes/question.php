28c67f2b7aa6158993742daa975afca2fef52418
Morgan Harris <gormster@me.com>
2015-10-13 15:59:11 +1100

    Getting started with contexts
    Writing some code for my template contexts

--- local/teameval/question/likert/classes/question.php
+++ local/teameval/question/likert/classes/question.php
@@ -5,21 +5,33 @@ namespace teamevalquestion_likert;
 class question implements \local_teameval\question {
     
     protected $id;
-
-    protected $test;
-
     protected $cm;
+    protected $title;
+    protected $description;
+    protected $minval;
+    protected $maxval;
 
     public function __construct($cmid, $questionid = null) {
         global $DB;
         $record = $DB->get_record('teamevalquestion_likert', array("id" => $questionid));
 
-        $this->test = $record->test;
-        $this->id = $questionid;
+        $this->id               = $questionid;
+        $this->title            = $record->title;
+        $this->description      = $record->description;
+        $this->minval           = $record->minval;
+        $this->maxval           = $record->maxval;
     }
     
     public function submission_view($userid) {
-        return array("id" => $this->id, "test" => $this->test);
+        global $DB;
+
+        $context = ["id" => $this->id, "title" => $this->title, "description" => $this->description];
+        
+        // get any response this user has given already
+        $response = response($this->id, $userid);
+        $marks = $response->raw_marks();
+
+        return $context;
     }
     
     public function editing_view() {
--- local/teameval/question/likert/classes/response.php
+++ local/teameval/question/likert/classes/response.php
@@ -3,13 +3,36 @@
 namespace teamevalquestion_likert;
 
 class response implements \local_teameval\response {
+
+	protected $questionid;
+	protected $userid;
+	protected $responses;
+
     public function __construct($questionid, $userid, $responseid = null) {
-        //todo
+        global $DB;
+
+    	$records = $DB->get_records("teamevalquestion_likert_resp", array("questionid" => $questionid, "fromuser" => $userid), '', 'id,touser,mark,markdate');
+
+    	//rearrange responses to be keyed by touser
+    	$this->responses = [];
+    	foreach($records as $r) {
+    		$this->responses[$r->touser] = $r;
+    	}
+    	
     }
     
     public function update_response($formdata) {
         //todo
     }
+
+    public function raw_marks() {
+    	$context = [];
+    	foreach($this->responses as $k => $r) {
+    		$context[$k] = $r->mark;
+    	}
+    	return $context;
+    }
+
 }
 
 ?>
