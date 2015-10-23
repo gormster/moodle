b0a6601809d22341422a847d88bae9ae12349a4e
Morgan Harris <gormster@me.com>
2015-10-23 14:58:23 +1100

    Starting questionnaire submission
    Submit questionnaires

---
+++ local/teameval/amd/src/submitquestion.js
@@ -0,0 +1,51 @@
+/*
+ * @package    local_teameval
+ * @copyright  2015 Morgan Harris
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+ /**
+  * Submit questionnaire button for teameval blocks
+  * @module local_teameval/submitquestion
+  */
+define(['jquery', 'core/templates', 'core/ajax', 'core/notification'], function($, templates, ajax, notification) {
+
+	var _cmid;
+
+	return {
+
+		submit: function(evt) {
+
+			var questions = $('#local-teameval-questions');
+			var promises = [];
+			questions.find('.question-container').each(function() {
+
+				var uiblocker = $('<div class="ui-blocker" />');
+				$(this).append(uiblocker);
+				var p = $(this).triggerHandler("submit");
+				promises.push(p);
+				p.always(function () {
+					uiblocker.remove();
+				});
+
+			});
+
+		}
+
+		initialise: function(cmid) {
+			
+			_cmid = cmid;
+
+			templates.render('local_teameval/submit_buttons', {}).done(function(html, js) {
+				var questionContainer = $('#local-teameval-questions');
+				questionContainer.append(html);
+				templates.runTemplateJS(js);
+
+				$('.local-teameval-submit-buttons .submit').click(this.submit);
+			}.bind(this));
+
+		}
+
+	}
+
+});
