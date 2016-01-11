96f2db98f8d4e0f5a04bc9d4200b4a9f8c901538
gormster <gormster@me.com>
2016-01-11 16:27:12 +1100

    Implement responses report

--- local/teameval/report/responses/classes/output/responses_report.php
+++ local/teameval/report/responses/classes/output/responses_report.php
@@ -8,14 +8,57 @@ use user_picture;
 
 class responses_report implements \renderable, \templatable {
 
-    public $scores;
+    protected $responses;
 
-    public function __construct() {
-
+    public function __construct($responses) {
+    	$this->responses = $responses;
     }
 
     public function export_for_template(\renderer_base $output) {
-        return [];
+    	$c = new stdClass;
+
+    	$c->questions = [];
+
+    	foreach($this->responses as $question) {
+    		$q = new stdClass;
+    		$q->title = $question->questioninfo->question->get_title();
+    		$q->groups = [];
+
+    		foreach($question->groups as $groupinfo) {
+    			$g = new stdClass;
+    			$g->name = $groupinfo->group->name;
+    			$g->markers = [];
+
+    			$g->marks = [];
+
+    			foreach($groupinfo->members as $m) {
+    				$marked = new stdClass;
+    				$marked->fullname = fullname($m->user);
+    				$g->marked[] = $marked;
+
+    				$marks = [];
+    				foreach($groupinfo->members as $n) {
+    					
+    					$marks[] = $m->response->opinion_of_readable($n->user->id);
+	    				
+    				}
+
+    				$marker = new stdClass;
+    				$marker->marker = fullname($m->user);
+    				$marker->scores = $marks;
+
+    				$g->marks[] = $marker;
+    				
+
+    			}
+
+    			$q->groups[] = $g;
+    		}
+
+    		$c->questions[] = $q;
+    	}
+
+        return $c;
     }
 
 }
--- local/teameval/report/responses/classes/report.php
+++ local/teameval/report/responses/classes/report.php
@@ -4,6 +4,9 @@ namespace teamevalreport_responses;
 
 require_once("{$CFG->dirroot}/local/teameval/lib.php");
 
+use user_picture;
+use stdClass;
+
 class report implements \local_teameval\report {
 
     protected $teameval;
@@ -15,8 +18,52 @@ class report implements \local_teameval\report {
     }
 
     public function generate_report() {
-        $scores = $this->teameval->multipliers_for_group(4);
-        return new output\responses_report($scores);
+        $questions = $this->teameval->get_questions();
+        $allgroups = $this->teameval->get_evaluation_context()->all_groups();
+
+        $responses = [];
+
+        // this will end up looking like:
+        // [ questioninfo => questioninfo, groups => [
+        //   groupid => [ group => group, members => [ userid => [user => user, response => response]]
+        // ]]
+
+        foreach($questions as $q) {
+        	$responseinfo = new stdClass;
+        	$responseinfo->questioninfo = $q;
+        	$responseinfo->groups = [];
+        	$responses[] = $responseinfo;
+        }
+
+        $groupmembers = [];
+
+        foreach($allgroups as $gid => $grp) {
+        	$groupmembers[$gid] = groups_get_members($gid, user_picture::fields('u'));
+        }
+
+        foreach($responses as $r) {
+        	foreach($allgroups as $gid => $grp) {
+	        	$groupinfo = new stdClass;
+	    		$groupinfo->group = $grp;
+	    		$groupinfo->members = [];
+
+	        	foreach($groupmembers[$gid] as $uid => $user) {
+	        		$memberinfo = new stdClass;
+	        		$memberinfo->user = $user;
+	        		
+        			$qi = $r->questioninfo;
+        			$cls = $qi->plugininfo->get_response_class();
+        			$resp = new $cls($this->teameval, $qi->question, $uid);
+        			$memberinfo->response = $resp;
+	        		
+	        		$groupinfo->members[$uid] = $memberinfo;
+	        	}
+
+	        	$r->groups[] = $groupinfo;
+	        }
+        }
+
+        return new output\responses_report($responses);
     }
 
 
