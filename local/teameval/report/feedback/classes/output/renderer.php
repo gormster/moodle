3346c284069a15430e9c4172826ffe3ff1444aa9
gormster <gormster@me.com>
2016-01-28 13:21:13 +1100

    yeah thing again

--- local/teameval/report/feedback/classes/output/renderer.php
+++ local/teameval/report/feedback/classes/output/renderer.php
@@ -9,7 +9,7 @@ class renderer extends plugin_renderer_base {
     public function render_feedback_report(feedback_report $report) {
 
         $data = $report->export_for_template($this);
-        return parent::render_from_template('teamevalreport_feedback/feedback_report', $data);
+        return parent::render_from_template('teamevalreport_feedback/report', $data);
 
     }
 
