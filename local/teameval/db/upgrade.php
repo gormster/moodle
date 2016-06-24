533d60021b794e036256e93eb96ae5adee6b606f
gormster <gormster@me.com>
2016-06-24 19:52:51 +1000

    Added unique key to qtype,questionid

--- local/teameval/db/upgrade.php
+++ local/teameval/db/upgrade.php
@@ -205,6 +205,21 @@ function xmldb_local_teameval_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2016061700, 'local', 'teameval');
     }
 
+    if ($oldversion < 2016062400) {
+
+        // Define field id to be added to teameval_questions.
+        $table = new xmldb_table('teameval_questions');
+        $field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+
+        // Conditionally launch add field id.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Teameval savepoint reached.
+        upgrade_plugin_savepoint(true, 2016062400, 'local', 'teameval');
+    }
+
 
 
     return true;
