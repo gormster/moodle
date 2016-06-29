b6a47b55952ae61a96533b618c53bef3d6a110cf
gormster <gormster@me.com>
2016-06-29 14:02:24 +1000

    Collapsible: multiple calls on a page
    Even though you shouldn’t be able to, you can actually have multiple
    templates blocks on one page thanks to the way blocks page filters
    work. So, here’s a patch that was causing events to be called twice
    (and toggling collapsibles closed as soon as they were opened).

--- blocks/teameval_templates/amd/src/collapsible.js
+++ blocks/teameval_templates/amd/src/collapsible.js
@@ -11,15 +11,27 @@ define(['jquery'], function($) {
 
 		init: function(settings) {
 			var S = $.extend({}, defaultSettings, settings);
-			$(S.selector).on('click', S.target, function(evt){
-				var collapser = $(evt.delegateTarget);
-				if (collapser.hasClass(S.expanded)) {
-					collapser.removeClass(S.expanded);
-					collapser.addClass(S.collapsed);
-				} else {
-					collapser.removeClass(S.collapsed);
-					collapser.addClass(S.expanded);
+			$(S.selector).each(function() {
+				// this can sometimes be called multiple times on the same element
+				// we want to ignore subsequent calls
+				var initialised = $(this).data('collapsible-initialised');
+
+				if (initialised) {
+					return;
 				}
+
+				$(this).data('collapsible-initialised', true);
+				$(this).on('click', S.target, function(evt){
+					var collapser = $(evt.delegateTarget);
+					if (collapser.hasClass(S.expanded)) {
+						collapser.removeClass(S.expanded);
+						collapser.addClass(S.collapsed);
+					} else {
+						collapser.removeClass(S.collapsed);
+						collapser.addClass(S.expanded);
+					}
+				});
+
 			});
 		}
 
