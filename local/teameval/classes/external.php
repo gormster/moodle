b02cd30ae69fe28f76949050fe7b06785cc05a54
gormster <gormster@me.com>
2016-12-09 20:50:08 +1100

    Fix no context bug for reports

--- local/teameval/classes/external.php
+++ local/teameval/classes/external.php
@@ -152,7 +152,9 @@ class external extends external_api {
     public static function report($cmid, $plugin) {
         global $USER, $PAGE;
 
-        team_evaluation::guard_capability(['cmid' => $cmid], ['local/teameval:viewallteams'], ['must_exist' => true]);
+        $context = team_evaluation::guard_capability(['cmid' => $cmid], ['local/teameval:viewallteams'], ['must_exist' => true]);
+
+        $PAGE->set_context($context);
 
         $teameval = team_evaluation::from_cmid($cmid);
         $teameval->set_report_plugin($plugin);
